ad65111a104ef4d9f06e66ae79e13538ec9b7c47
James Westervelt <james.a.westervelt@gmail.com>
2018-12-02 20:38:11 -0800

    Added question js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,40 @@
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+
+var triviaQuestions = [
+    {
+        question: "Finish the phrase: Let's get _______"
+        answers: {
+            a: "Ugly"
+            b: "Money"
+            c: "Sickening"
+        }
+        correctAnswer: "c"
+    },
+    {
+        question: "Which one is not a winning queen of RPDR?"
+        answers: {
+            a: "Sharon Needles"
+            b: "Aquaria"
+            c: "Jiggly Caliente"
+        }
+        correctAnswer: "c"
+    },
+    {
+        question: "What phrase has been removed from RPDR for being too controversial?"
+        answers: {
+            a: "You've got She-mail"
+            b: "Condragulations!"
+            c: "That's a man Maury!"
+        }
+        correctAnswer: "a"
+    },
+    {
+        question: "Fill in the blank: Chante you stay. ___________"
+        answers: {
+            a: "Bye gurl bye"
+            b: "Cha-se away"
+            c: "Miss Vangie"
+        }
+        correctAnswer: "b"
+    },
+]
